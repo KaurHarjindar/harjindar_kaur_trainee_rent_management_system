909aac631594d1efb9aab73efa9da19e88c318d6
KaurHarjindar <harjindkaur8@gmail.com>
2020-01-06 12:54:35 +0530

    [IMP] cmt: added a new module

---
+++ rms/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name' : 'Rent Management system',
+    'version' : '1.1',
+    'summary' : 'manage the tenants',
+    'data' : [
+        'security/ir.model.access.csv',
+       	'views/firstView.xml'
+    ],
+}
