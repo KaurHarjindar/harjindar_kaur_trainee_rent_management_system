18a4bc2d711a5afdb1c3b82ae1ae9d57ed8ce0c1
KaurHarjindar <harjindkaur8@gmail.com>
2020-01-07 13:01:57 +0530

    [IMP] cmt: chenges in view

--- rms/models/firstModel.py
+++ rms/models/firstModel.py
@@ -6,15 +6,33 @@ class owner(models.Model):
     _description = "owner record"
     _rec_name="owner_name"
 
+    @api.depends
+    def button_start(self):
+       for rec in self:
+           rec.write({'state': 'Start'})
+    @api.depends
+    def button_running(self):
+        for rec in self:
+            rec.write({'state': 'Running'})
+    @api.depends
+    def button_Stop(self):
+        for rec in self:
+            rec.write({'state': 'Complete'})
+
 
     owner_id = fields.Char(String="owner_id")
     owner_name = fields.Char(String="owner_name")
     email = fields.Text(String="owner_email")
     password = fields.Text(String="owner_password")
+    state=fields.Selection([
+        ('Start','Start'),
+        ('Running','Running'),
+        ('Complete','Complete'),
+        ],string="state",default="Start",readonly=True)
 
 class tenant(models.Model):
     _name="training.tenant"
-    _description="tenant table"
+    _description="tenant table"  
     _rec_name="tenant_name"
 
     @api.constrains('phone')
@@ -38,6 +56,7 @@ class tenant(models.Model):
     Occupation=fields.Selection([('Study', 'Study'),('Job','Job')],String="Occupation")
     Joining_date=fields.Date(String="Check-In date")
     room_ids=fields.Many2one("training.room",String="room ids")
+    
 
 class room(models.Model):
     _name="training.room"
@@ -90,4 +109,5 @@ class roomallotment(models.Model):
     allotOut_date=fields.Date(String="Check-out Date")
     Total_rent=fields.Integer(String="Total Rent",compute="set_rent_month",store=True)
     Paymnent=fields.Selection([('1','1'),('3','3'),('6','6'),('12','12')],String="Paymnent Months")
+    
 
